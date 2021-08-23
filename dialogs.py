0ba020014da029d54a3f9bedbc262ff310ef396e
aplneto <apln2@cin.ufpe.br>
2021-08-22 22:59:12 -0300

    Framework concluded

--- dialogs.py
+++ dialogs.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+import pandas as pd
 
 from intents.openinggreeting import OpeningGreeting
 from collections import defaultdict
@@ -10,6 +11,9 @@ with open('dialogs.json') as _file:
     DIALOG_FLOW = json.loads(_file.read())
 
 TOLERANCE = 3
+
+claim_database = pd.read_csv('claims.csv')
+policy_database = pd.read_csv('policy.csv')
 
 class DialogManager(object):
     def __init__(self, user_name = '', current_intent = None, **kwargs):
@@ -49,7 +53,9 @@ class DialogManager(object):
         if self.__current_intent is None:
             self.__patience = 0
             self.__current_intent = intent
+            print("New intent: %s" % intent)
         self.__entities += [(x, y) for x, y in entities if x != 'O']
+
         if intent in ('outofdomain', 'contentonly') and self.__waiting_for_variable:
             actions = DIALOG_FLOW[self.__current_intent]["actions"]
             entity_needed = actions[self.__current_action][0]
@@ -69,22 +75,25 @@ class DialogManager(object):
         else:
             action = ''
             actions = DIALOG_FLOW[self.__current_intent]["actions"]
+            print(actions)
             for action_name in actions:
                 entities_needed = set(actions[action_name])
                 if entities_needed <= set([x for x, y in self.__entities]):
                     action = action_name
-                    break
+                    return self.__evaluate_action(action, self.__entities)
+            if self.__current_intent in DIALOG_ANSWERS:
+                return np.random.choice(DIALOG_ANSWERS[self.__current_intent])
             if len(action) == 0:
-                print(actions)
                 self.__possible_actions = [x for x in actions]
-                print(self.__possible_actions)
                 self.__current_action = self.__possible_actions.pop()
                 action = 'ask_for_confirmation'
         
         return self.__evaluate_action(action, self.__entities)
         
     def __evaluate_action(self, action, entities):
+        print(action)
         if action == 'greet':
+            self.__current_intent = None
             name = [y for x, y in self.__entities if x == 'name']
             name = ' '.join(name)
             return np.random.choice(DIALOG_ANSWERS['greetings']) % name
@@ -92,6 +101,14 @@ class DialogManager(object):
             return self.__ask_for_entities()
         elif action == 'ask_for_confirmation':
             return self.__ask_for_confirmation()
+        elif action == "open_claim":
+            pass
+        elif action == "search_claim_by_name":
+            return "Your claim status is: [OK]"
+        elif action == "search_claim_by_claimid":
+            return "There is no claim by that id number"
+        elif action == "search_claim_by_ssn":
+            return "Your claim status is: [OK]"
         else:
             return np.random.choice(DIALOG_ANSWERS['invalid'])
     
@@ -108,5 +125,7 @@ class DialogManager(object):
         return "Can you please tell me your %s?" % asking
     
     def __ask_for_confirmation(self):
-        action_name = DIALOG_FLOW[self.__current_intent]['alias'][self.__current_action]
+        action_name = self.__current_action
+        if action_name in DIALOG_FLOW[self.__current_intent]['alias']:
+            action_name = DIALOG_FLOW[self.__current_intent]['alias'][action_name]
         return "Do you want me to %s?" % action_name
